bed37a43767f7162e53e9fae42fcd36ed1b6b251
KubaTaba1uga <luna7337anul@proton.me>
2026-04-29 14:35:41 +0200

    fix bugs

--- buildrootdoc2set.py
+++ buildrootdoc2set.py
@@ -17,14 +17,20 @@ page = open(os.path.join(docpath,'manual.html')).read()
 soup = BeautifulSoup(page)
 
 any = re.compile('.*')
+paths = []
+names = []
 for tag in soup.find_all('a', {'href':any}):
     name = tag.text.strip()
     if len(name) > 1:
-        path = tag.attrs['href'].strip()
-        if path != 'manual.html':
-            path = f"buildroot/{path}"
+        path = tag.attrs['href']
+        if path in paths or name in names:
+            continue
+        if path != 'manual.html' and "#" in path and not "http" in path and not name.startswith("["):
+            path = f"manual.html{path}"
             cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, 'func', path))
             print('name: %s, path: %s' % (name, path))
+            paths.append(path)
+            names.append(name)
 
 conn.commit()
 conn.close()
