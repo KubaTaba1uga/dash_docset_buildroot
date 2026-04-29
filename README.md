e51c3a96cbf3efdd95b60a24579ee28e72dbe47b
KubaTaba1uga <luna7337anul@proton.me>
2026-04-29 14:25:00 +0200

    init repo

---
+++ README.md
@@ -0,0 +1,3 @@
+## Dash Docset Buildroot
+
+Buildroot docset for Dash.
---
+++ buildrootdoc2set.py
@@ -0,0 +1,30 @@
+#!/usr/local/bin/python
+
+import os, re, sqlite3
+from bs4 import BeautifulSoup 
+
+conn = sqlite3.connect('buildroot.docset/Contents/Resources/docSet.dsidx')
+cur = conn.cursor()
+
+try: cur.execute('DROP TABLE searchIndex;')
+except: pass
+cur.execute('CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);')
+cur.execute('CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);')
+
+docpath = 'buildroot.docset/Contents/Resources/Documents/'
+
+page = open(os.path.join(docpath,'manual.html')).read()
+soup = BeautifulSoup(page)
+
+any = re.compile('.*')
+for tag in soup.find_all('a', {'href':any}):
+    name = tag.text.strip()
+    if len(name) > 1:
+        path = tag.attrs['href'].strip()
+        if path != 'manual.html':
+            path = f"buildroot/{path}"
+            cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, 'func', path))
+            print('name: %s, path: %s' % (name, path))
+
+conn.commit()
+conn.close()
